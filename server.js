011301444046baaec89cb56338ccc760fb04b4d3
dowon <dowonkim0612@naver.com>
2026-03-31 01:03:09 +0900

    fix: Express 5 wildcard route 문법 수정

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use('/api/tmap', createProxyMiddleware({
 app.use(express.static(join(__dirname, 'dist')))
 
 // SPA 라우팅 — 모든 경로를 index.html로
-app.get('*', (_, res) => {
+app.get('/{*path}', (_, res) => {
   res.sendFile(join(__dirname, 'dist', 'index.html'))
 })
 
